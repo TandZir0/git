a911292933e185afe4741e15d893b7f45f7fd30b
TandZir0 <titovsit1980@gmail.com>
2023-10-14 23:45:45 +0500

    new file:   "2 \321\201\320\273\320\260\320\271\320\264/2 slaid.html" 	new file:   "2 \321\201\320\273\320\260\320\271\320\264/2 style.css"     new file:   "2 \321\201\320\273\320\260\320\271\320\264/script.js"     new file:   "2 \321\201\320\273\320\260\320\271\320\264/trach.jpg"     new file:   html/image/7517_falcade-bl-italy_1920x1080.jpg     new file:   html/index.html     new file:   web program/free-icon-font-facebook-6422199.png     new file:   web program/free-icon-font-github-6424084.png     new file:   web program/free-icon-font-twitter-6422210.png     new file:   web program/free-icon-font-vk-6422212.png     new file:   web program/free-icon-mail-646135.png 	new file:   web program/html.html 	new file:   web program/index.css 	new file:   web program/skaly_sneg_ozero_913071_1920x1080.jpg 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/free-icon-font-facebook-6422199.png" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/free-icon-font-github-6424084.png" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/free-icon-font-twitter-6422210.png" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/free-icon-font-vk-6422212.png" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/free-icon-mail-646135.png" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/index.html" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/steles.css"     new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/style.css" 	new file:   "\320\277\320\265\321\200\320\262\321\213\320\271 \321\201\320\273\320\260\320\271\320\264/web.html"

---
+++ 2 слайд/script.js
@@ -0,0 +1,32 @@
+const inputEl = document.getElementById('example')
+const btmEl = document.querySelector('.todo-list__but')
+const fielEl = document.querySelector('.todo-list__field')
+
+function onButClick() {
+    if (inputEl.value.length) {
+        const divEl = document.createElement('div')
+        divEl.classList.add('todo-list__item')
+
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+
+        const text = document.createElement('p')
+        text.classList.add('todo-list__item-text')
+        text.innerText = inputEl.value
+
+        const img = document.createElement('img')
+        img.src = 'trach.jpg'
+
+        divEl.appendChild(checkbox)
+        divEl.appendChild(text)
+        divEl.appendChild(img)
+
+        fielEl.appendChild(divEl)
+        inputEl.value = ''
+    }
+}
+btmEl.addEventListener('click', onButClick)
+
+inputEl.addEventListener('input', () => {
+    console.log(inputEl.value);
+})
